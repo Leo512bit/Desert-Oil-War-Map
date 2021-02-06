685edfd1da1cd5a31551f910fb00af32e1394854
Leo512bit <64435704+Leo512bit@users.noreply.github.com>
2021-02-06 14:54:48 -0800

    Add files via upload

--- mission.lua
+++ mission.lua
@@ -1,1 +1,1 @@
-Media.DisplayMessage("Please before you leave go to the pause menu and go to the briefing tab to read the map rules. Thank you for playing my map :) (the map is incomplete)")
+Media.DisplayMessage("Please go to the briefing tab to read the map rules this map is very different with a new economy. Thank you for playing my map :) (the map is incomplete)")
