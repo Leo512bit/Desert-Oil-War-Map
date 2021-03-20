a333a94d9a9d471b46c538d43e33c495979c0067
Leo512bit <leonardmatthewteyssier@gmail.com>
2021-03-19 19:01:25 -0700

    New R

--- mission.lua
+++ mission.lua
@@ -1,1 +1,28 @@
-Media.DisplayMessage("Please go to the briefing tab to read the map rules this map is very different with a new economy. Thank you for playing my map :) (the map is incomplete)")
+
+WorldLoaded = function()
+	Media.DisplayMessage("Please go to the briefing tab to read the map rules this map is very different with a new economy. Thank you for playing my map :) (the map is incomplete)")
+end
+
+ColorChanger = function()
+	if RedLight < DesiredRed then
+		RedLight = RedLight + 0.001
+	elseif RedLight > DesiredRed then
+		RedLight = RedLight - 0.001
+	end
+	if GreenLight < DesiredGreen then
+		GreenLight = GreenLight + 0.001
+	elseif GreenLight > DesiredGreen then
+		GreenLight = GreenLight - 0.001
+	end
+	if BlueLight < DesiredBlue then
+		BlueLight = BlueLight + 0.001
+	elseif BlueLight > DesiredBlue then
+		BlueLight = BlueLight - 0.001
+	end
+	Lighting.Red = RedLight
+	Lighting.Green = GreenLight
+	Lighting.Blue = BlueLight
+	Trigger.AfterDelay(ColorChangeSpeed, function()
+		ColorChanger()
+	end)
+end
